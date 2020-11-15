8a3d7c96f1cc1ed697a4249acdca85282865249c
Damian Martinez <damian.martinezd@softtek.com>
2020-11-15 04:58:01 -0600

    adding

--- App.js
+++ App.js
@@ -8,38 +8,70 @@
 
 import React from 'react';
 import {
+  SafeAreaView,
   StyleSheet,
+  ScrollView,
   View,
   Text,
+  StatusBar,
 } from 'react-native';
 
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-export default class MyHomeApp extends Component<Props> {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <View style={styles.body}>
-        <Text style={styles.sectionTitle}>HOME</Text>
-        <Text>
-        Before we jump into Electrode Native Navigation, it’s essential to understand what Electrode Native is, and why we have created this framework. Electrode Native allows for the combination of multiple MiniApps (React Native applications) into a single container. The container is generated entirely by the Electrode Native platform and is packaged as an AAR library for Android and as a Framework for iOS (these libraries are similar to standard third-party libraries that Android and iOS developers are familiar with).
-        </Text>
-    </View>
-    )
-  }
-}
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.body}>
+      <Text style={styles.sectionTitle}>HOME PAGE</Text>
+      <Text>
+        The Electrode Native container HOME
+      </Text>
+  </View>
+  )
+};
 
 const styles = StyleSheet.create({
-
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
   body: {
-    backgroundColor: "#65DD33",
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
-    color: "#CCDFD4",
-  }
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
 });
 
+export default App;
